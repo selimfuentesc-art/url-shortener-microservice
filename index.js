f957f4edd25fe06cd255c9deb99ac883758db2ae
selimfuentesc-art <selim.fuentesc@gmail.com>
2026-05-24 19:21:52 -0400

    Proyecto URL shortener terminado

---
+++ index.js
@@ -0,0 +1,91 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const dns = require('dns');
+const { URL } = require('url');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+const port = process.env.PORT || 3000;
+
+// Página principal
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Base de datos temporal
+let urls = [];
+let counter = 1;
+
+// Crear URL corta
+app.post('/api/shorturl', function(req, res) {
+
+  const originalUrl = req.body.url;
+
+  let parsedUrl;
+
+  try {
+    parsedUrl = new URL(originalUrl);
+  } catch (err) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  if (
+    parsedUrl.protocol !== 'http:' &&
+    parsedUrl.protocol !== 'https:'
+  ) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  dns.lookup(parsedUrl.hostname, (err) => {
+
+    if (err) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    const shortUrl = counter++;
+
+    urls.push({
+      original_url: originalUrl,
+      short_url: shortUrl
+    });
+
+    res.json({
+      original_url: originalUrl,
+      short_url: shortUrl
+    });
+
+  });
+
+});
+
+// Redirección
+app.get('/api/shorturl/:short_url', function(req, res) {
+
+  const shortUrl = parseInt(req.params.short_url);
+
+  const found = urls.find(
+    item => item.short_url === shortUrl
+  );
+
+  if (!found) {
+    return res.json({
+      error: 'No short URL found'
+    });
+  }
+
+  res.redirect(found.original_url);
+
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
